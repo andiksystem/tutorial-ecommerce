0ff2646ca367a0a5bd776b87a0083901ec52c719
Andik Hermawan <andik.system@gmail.com>
2023-01-30 23:51:24 +0700

    part16

--- src/main/java/com/andik/ecommerce/model/JwtResponse.java
+++ src/main/java/com/andik/ecommerce/model/JwtResponse.java
@@ -11,16 +11,19 @@ public class JwtResponse implements Serializable {
     private String type = "Bearer";
     private String username;
     private String email;
+    private String role;
 
     public JwtResponse(
             String accessToken,
             String refreshToken,
             String username,
-            String email) {
+            String email,
+            String role) {
         this.username = username;
         this.email = email;
         this.token = accessToken;
         this.refreshToken = refreshToken;
+        this.role = role;
     }
 
 }
